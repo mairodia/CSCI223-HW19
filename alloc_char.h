f704dd7171a385ba41471dddbeb7817a48ea0406
Jennifer King <32186812+mairodia@users.noreply.github.com>
2018-05-11 16:26:25 -0700

    Create alloc_char.h

---
+++ alloc_char.h
@@ -0,0 +1,21 @@
+// ============================================================================
+// File: alloc_char.h (Spring 2018)
+// ============================================================================
+// Programmer: Jennifer King
+// Date: 5/10/2018
+// Project: AllocArray
+// Professor: Scott Edwards
+// Class: CSCI 223 F | TR 4:30PM
+// ============================================================================
+
+#ifndef ALLOC_CHAR_H
+#define ALLOC_CHAR_H
+
+char*   Alloc_charArray(int  numElems);
+void    Init_charArray(char  *ptr, int  numElems, char  initVal);
+void    Disp_charArray(char  *ptr, int  numElems);
+
+#endif  // ALLOC_CHAR_H
+
+
+
