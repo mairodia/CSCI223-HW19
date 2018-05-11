975f32b8c49400733107be2bf778510b281b832b
Jennifer King <32186812+mairodia@users.noreply.github.com>
2018-05-11 16:29:29 -0700

    Create alloc_double.h

---
+++ alloc_double.h
@@ -0,0 +1,18 @@
+// ============================================================================
+// File: alloc_double.h (Spring 2018)
+// ============================================================================
+// Programmer: Jennifer King
+// Date: 5/10/2018
+// Project: AllocArray
+// Professor: Scott Edwards
+// Class: CSCI 223F | TR 4:30PM
+// ============================================================================
+
+#ifndef ALLOC_DOUBLE_H
+#define ALLOC_DOUBLE_H
+
+double* Alloc_doubleArray(int  numElems);
+void    Init_doubleArray(double  *ptr, int  numElems, double  initVal);
+void    Disp_doubleArray(double  *ptr, int  numElems);
+
+#endif  // ALLOC_DOUBLE_H
