e30e6e7ddc43e5cb5444674cf1ecca897ec1094f
Jennifer King <32186812+mairodia@users.noreply.github.com>
2018-05-11 16:21:58 -0700

    Create alloc_int.h

---
+++ alloc_int.h
@@ -0,0 +1,17 @@
+// ============================================================================
+// File: alloc_int.h (Spring 2018)
+// ============================================================================
+// Programmer: Jennifer King
+// Date: 5/10/2018
+// Project: AllocArray
+// Professor: Scott Edwards
+// Class: CSCI 223 | TR 4:30PM
+// ============================================================================
+#ifndef ALLOC_INT_H
+#define ALLOC_INT_H
+
+int*    Alloc_intArray(int numElems);
+void    Init_intArray(int *ptr, int numElems, int initVal);
+void    Disp_intArray(int *ptr, int numElems);
+
+#endif  // ALLOC_INT_H
